53719adf267e90d55306c838d63cf8bc70711a31
ilyakanavin <ilyakanavin9@gmail.com>
2024-03-25 23:13:48 +0800

    New

--- script.js
+++ script.js
@@ -1,117 +1,194 @@
-// Функция для получения случайного цвета в формате HEX
-const getRandomColor = () => "#" + Math.floor(Math.random() * 0xffffff).toString(16);
+const getRandomColor = () => {
+  return "#" + Math.floor(Math.random() * 16777211).toString(16);
+};
 
-// Функция для генерации массива случайных цветов
-// Принимает количество цветов в массиве
-const getArrayOfRandomColors = (count) => Array.from({ length: count }, getRandomColor);
-
-const levelsOfDifficulty = {
+const difficulty = {
   easy: {
     size: 3,
     count: 6,
     time: 80,
-    colors: getArrayOfRandomColors(2),
+    colors: [getRandomColor(), getRandomColor()],
   },
   medium: {
-    size: 4,
-    count: 8,
-    time: 80,
-    colors: getArrayOfRandomColors(3),
+    size: 6,
+    count: 12,
+    time: 120,
+    colors: [
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+    ],
   },
   hard: {
-    size: 5,
-    count: 10,
-    time: 80,
-    colors: getArrayOfRandomColors(4),
-  }
+    size: 9,
+    count: 16,
+    time: 180,
+    colors: [
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+    ],
+  },
+  impossible: {
+    size: 22,
+    count: 25,
+    time: 200,
+  },
 };
+
+class SoundEffect {
+  constructor() {
+    if (SoundEffect.instance) {
+      return SoundEffect.instance;
+    } else {
+      SoundEffect.instance = this;
+      this.wrong_ = new Audio("./sounds/wrong.mp3");
+
+      this.cool_ = new Audio("./sounds/cool.mp3");
+    }
+  }
+
+  wrong = () => {
+    this.wrong_.volume = 0.5;
+    this.wrong_.play();
+  };
+
+  cool = () => {
+    this.cool_.volume = 0.5;
+    this.cool_.play();
+  };
+}
+new SoundEffect();
 
 class Cube {
   constructor(index, size, chooseCubeCallback, rotate = 0) {
-    this.index = index; // Индекс куба
-    this.size = size; // Размер куба
+    this.index = index;
+    this.size = size;
 
-    // Создание элемента куба
     this.cube = document.createElement("div");
     this.cube.classList.add("cube");
     this.cube.style.transform = `rotate${rotate}`;
 
-    // Обработчик правого клика (контекстного меню) для вращения куба
     this.cube.addEventListener("contextmenu", (e) => {
       e.preventDefault();
-      const rotateValue = parseInt(this.cube.style.transform.slice(7, -4) || 0); // Получаем текущий угол вращения
-      this.cube.style.transform = `rotate(${rotateValue + 20}deg)`; // Вращаем на 20 градусов
+      const rotate = this.cube.style.transform;
+      const rotate_ = rotate.split("(")[1].split(")")[0];
+      const rotate__ = parseInt(rotate_);
+      this.cube.style.transform = `rotate(${rotate__ + 20}deg)`;
     });
 
-    // Добавление контента в куб
     const content = document.createElement("div");
     content.classList.add("cube__content");
     this.cube.appendChild(content);
-
-    // Заполнение куба элементами
     for (let i = 0; i < size; i++) {
       const row = document.createElement("div");
       row.classList.add("cube__row");
+
       for (let j = 0; j < size; j++) {
         const div = document.createElement("div");
         div.classList.add("cube__item");
+        div.style.backgroundColor =
+          "#" + Math.floor(Math.random() * 16777215).toString(16);
         const color = Math.floor(Math.random() * 16777215).toString(16);
-        div.style.backgroundColor = `#${color === "ffffff" ? "000000" : color}`;
+        div.style.backgroundColor =
+          color === "ffffff" ? "#000000" : "#" + color;
         row.appendChild(div);
       }
+
+      this.cube.style.top = index / size + "%";
       content.appendChild(row);
     }
 
-    this.cube.style.top = `${index / size}%`; // Позиционирование куба
-
-    // Обработчик клика
     this.cube.addEventListener("click", chooseCubeCallback);
+    this.dragElement(this.cube);
   }
 
-  // Метод для добавления куба в DOM
   appendToNode = (node) => node.appendChild(this.cube);
 
-  // Метод для установки предварительного размера куба и его элементов
   previewSize = () => {
     this.cube.style.width = "100px";
     this.cube.style.height = "100px";
     const divs = this.cube.getElementsByClassName("cube__item");
-    [...divs].forEach(div => {
-      div.style.width = `${100 / this.size}px`;
-      div.style.height = `${100 / this.size}px`;
-    });
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].style.width = `${100 / this.size}px`;
+      divs[i].style.height = `${100 / this.size}px`;
+    }
   };
 
+  dragElement = (elmnt) => {
+    var pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
+    elmnt.addEventListener("mousedown", dragMouseDown);
+    function dragMouseDown(e) {
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+
+    const elementDrag = (e) => {
+      e = e;
+      e.preventDefault();
+      elmnt.style.position = "absolute";
+
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+
+      // calculate new position
+      var newTop = elmnt.offsetTop - pos2;
+      var newLeft = elmnt.offsetLeft - pos1;
+
+      elmnt.style.top = newTop + "px";
+      elmnt.style.left = newLeft + "px";
+    };
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  };
 }
 const style = 0;
 
 class Game {
   constructor(cubeCount, cubeSize, trigger, rotate = false) {
-    this.cubeCount = cubeCount; // Количество кубов
-    this.cubeSize = cubeSize; // Размер кубов
-    this.cubes = []; // Массив для хранения кубов
+    this.cubeCount = cubeCount;
+    this.cubeSize = cubeSize;
+    this.cubes = [];
 
-    this.trigger = trigger; // Функция обратного вызова
-    this.board = document.getElementById("game-board"); // Игровое поле
-    this.rotate = rotate; // Флаг вращения
-    this.reset(); // Очистка игрового поля
-    this.initializeBoard(); // Инициализация игрового поля
+    this.trigger = trigger;
+    this.board = document.getElementById("game-board");
+    this.rotate = rotate;
+    this.reset();
+    this.initailizeBoard();
 
-    // Индекс выигрышного куба выбирается случайно
-    this.gameWinningCubeIndex = Math.floor(Math.random() * cubeCount);
+    const randomCube = Math.floor(Math.random() * cubeCount);
+
+    this.gameWinningCubeIndex = randomCube;
   }
 
-  initializeBoard = () => {
+  initailizeBoard = () => {
     const chooseCubeCallback = (idx) => {
       return () => {
-        // Проверка, является ли куб выигрышным
         if (idx === this.gameWinningCubeIndex) {
+          new SoundEffect().cool();
           this.trigger(true);
         } else {
           this.trigger(false);
+          new SoundEffect().wrong();
           this.cubes[idx].cube.classList.add("wrong");
 
-          // Удаление класса "wrong" через 1 секунду
           setTimeout(() => {
             this.cubes[idx].cube.classList.remove("wrong");
           }, 1000);
@@ -119,191 +196,167 @@ class Game {
       };
     };
 
-    // Создание и добавление кубов на игровое поле
     for (let i = 0; i < this.cubeCount; i++) {
-      const rotateDeg = this.rotate ? `(${Math.floor(Math.random() * 90)}deg)` : "(0deg)";
-      const cube = new Cube(i, this.cubeSize, chooseCubeCallback(i), rotateDeg);
+      const rotate = this.rotate
+        ? `(${Math.floor(Math.random() * 90)}deg)`
+        : "(0deg)";
 
+      const cybe = new Cube(i, this.cubeSize, chooseCubeCallback(i), rotate);
       if (this.cubeCount >= 50) {
-        cube.cube.style.width = "8%";
-        cube.cube.style.padding = "0";
-        cube.cube.style.paddingBottom = "8%";
+        cybe.cube.style.width = "8%";
+        cybe.cube.style.padding = "0";
+        cybe.cube.style.paddingBottom = "8%";
       }
-
-      this.cubes.push(cube);
+      this.cubes.push(cybe);
     }
 
-    this.cubes.forEach(cube => cube.appendToNode(this.board));
+    for (let cube of this.cubes) {
+      cube.appendToNode(this.board);
+    }
   };
 
-  reset = () => {
-    this.board.innerHTML = ""; // Очистка игрового поля
-  };
+  reset = () => (this.board.innerHTML = "");
 }
 
 class ButtonSelector {
   constructor(texts, styles = {}) {
-    this.btns = []; // Массив для хранения кнопок
-    this.state = null; // Текущее состояние (выбранная кнопка)
-    this.appender = document.createElement("div");
-    this.appender.className = "button-selector";
+    this.btns = [];
+    this.state = null;
+    const appender = document.createElement("div");
 
-    // Создание кнопок на основе предоставленных текстов и стилей
-    texts.forEach((t, i) => {
+    this.appender = appender;
+    this.appender.className = "button-selector";
+    for (let [i, t] of texts.entries()) {
       const btn = document.createElement("button");
       btn.innerHTML = t;
-      btn.style = styles[t]; // Применение стилей к кнопке
+      btn.style = styles[t];
       btn.className = "picker";
       btn.value = t;
 
-      this.appender.appendChild(btn);
+      appender.appendChild(btn);
       this.btns.push(btn);
-    });
+    }
 
-    // Добавление обработчика клика для каждой кнопки
-    this.btns.forEach(btn => {
+    for (let btn of this.btns) {
       btn.addEventListener("click", (e) => {
         e.preventDefault();
         this.state = e.target.value;
-        this.btns.forEach(btn => btn.classList.remove("active"));
+        for (let btn of this.btns) {
+          btn.classList.remove("active");
+        }
+        this.state = e.target.value;
         e.target.classList.add("active");
       });
-    });
+    }
   }
 
-  appendTo = (nodeId) => {
+  appendTo = (nodeId) =>
     document.getElementById(nodeId).appendChild(this.appender);
-  };
 }
 
 class Input {
   constructor() {
-    // Создание элемента input и настройка его свойств
     this.input = document.createElement("input");
     this.input.className = "input";
     this.input.type = "text";
-    this.input.placeholder = "Введите ваше имя";
+    this.input.placeholder = "Enter your name";
 
-    // Слушатель изменений значения input
     this.input.addEventListener("change", (e) => {
-      this.state = e.target.value; // Обновление состояния при изменении input
+      this.state = e.target.value;
     });
   }
 
-  // Метод для добавления input в DOM
-  appendTo = (nodeId) => document.getElementById(nodeId).appendChild(this.input);
+  appendTo = (nodeId) =>
+    document.getElementById(nodeId).appendChild(this.input);
 }
-
 
 class State {
   constructor() {
-    this.listeners = []; // Список слушателей состояния
+    this.listeners = [];
   }
 
-  // Установка нового состояния и оповещение слушателей
   setState = (state) => {
     this.state = state;
     this.notify();
   };
 
-  // Получение текущего состояния
   getState = () => this.state;
 
-  // Добавление слушателей. Поддержка добавления как одного слушателя, так и массива слушателей
-  addListener = (listener) => {
-    if (Array.isArray(listener)) {
-      this.listeners.push(...listener);
+  addListener = (l) => {
+    if (l instanceof Array) {
+      this.listeners.push(...l);
     } else {
-      this.listeners.push(listener);
+      this.listeners.push(l);
     }
   };
 
-  // Оповещение всех слушателей о изменении состояния
-  notify = () => this.listeners.forEach(listener => listener(this));
+  notify = () => {
+    for (let listener of this.listeners) {
+      listener(this);
+    }
+  };
 }
-
 
 class Timer extends State {
   constructor(time = 180, listeners = []) {
-    super(); // Вызов конструктора базового класса
+    super();
 
-    this.interval = null; // Идентификатор интервала
-    this.addListener(listeners); // Добавление переданных слушателей
-    this.setState(time); // Инициализация таймера с начальным временем
+    this.interval = null;
+    this.addListener(listeners);
+    this.setState(time);
 
-    // Установка интервала уменьшения времени таймера каждую секунду
-    this.interval = setInterval(() => {
+    const interval = setInterval(() => {
       this.setState(this.state - 1);
       if (this.state <= 0) {
-        clearInterval(this.interval); // Очистка интервала при достижении 0
+        clearInterval(interval);
       }
     }, 1000);
+
+    this.interval = interval;
   }
 }
 
-// HTML-разметка для стартовой формы
 const startHTML = `
   <form id="start">
-    <button type="submit" id="start">Начать игру</button>
+    <button type="submit" id="start">Start</button>
   </form>
 `;
 
-
-class LocalStorage {
+class LocalSTORAGE {
   constructor() {
-    // Попытка извлечь таблицу лидеров из localStorage или инициализация пустым массивом
-    this.leaderboard = JSON.parse(localStorage.getItem("leaderboard")) || [];
-
-    // Сохранение начального состояния таблицы лидеров, если она была пустой
-    if (!this.leaderboard.length) {
+    this.leaderboard = [];
+    if (!localStorage.getItem("leaderboard")) {
       localStorage.setItem("leaderboard", JSON.stringify(this.leaderboard));
+    } else {
+      this.leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
     }
   }
 
-  sortAndTrim = () => {
-    // Сортировка по убыванию счета и фильтрация невалидных записей
+  sort = () => {
     this.leaderboard.sort((a, b) => b[1] - a[1]);
     this.leaderboard = this.leaderboard.filter((l) => l[0] !== "" && l[0]);
-
-    // Обрезка до первых 10 записей
     this.leaderboard = this.leaderboard.slice(0, 10);
-
-    // Сохранение обновленной таблицы лидеров в localStorage
     localStorage.setItem("leaderboard", JSON.stringify(this.leaderboard));
   };
 
   add = (res) => {
-    // Поиск существующей записи с таким же именем
-    const existingIndex = this.leaderboard.findIndex((l) => l[0] === res[0]);
-
-    if (existingIndex > -1) {
-      // Если счет новой записи больше, обновляем запись
-      if (this.leaderboard[existingIndex][1] < res[1]) {
-        this.leaderboard[existingIndex] = res;
-        this.sortAndTrim();
-      }
-      // В противном случае (счет меньше или равен), ничего не делаем
-    } else {
-      // Если такой записи нет, просто добавляем новую запись
-      this.leaderboard.push(res);
-      this.sortAndTrim();
-    }
+    this.leaderboard.push(res);
+    this.sort();
   };
 
   set = (board) => {
     this.leaderboard = board;
-    this.sortAndTrim();
+    this.sort();
   };
 }
-
 const endHTML = `
 <div id="end">
-  <h1>Игра окончена</h1>
+  <h1>Game over</h1>
   <p>Your score is: <span id='game-score'></span></p>
 </div>
 `;
 
-const ls = new LocalStorage();
+const ls = new LocalSTORAGE();
 
 class Error {
   constructor(message) {
@@ -349,7 +402,7 @@ class AppState {
     const input = new Input();
     input.appendTo("start_container");
 
-    const select = new ButtonSelector(Object.keys(levelsOfDifficulty));
+    const select = new ButtonSelector(Object.keys(difficulty));
     select.appendTo("start_container");
     const div = document.createElement("div");
     div.innerHTML = startHTML;
@@ -362,10 +415,10 @@ class AppState {
       if (input.state && input.state.length > 0) {
         this.state.name = input.state ?? "ANONYMOUS USER";
         this.state.difficulty = select.state;
-        this.state.size = levelsOfDifficulty[select.state].size;
-        this.state.time = levelsOfDifficulty[select.state].time;
+        this.state.size = difficulty[select.state].size;
+        this.state.time = difficulty[select.state].time;
 
-        this.cubeCount = levelsOfDifficulty[select.state].count;
+        this.cubeCount = difficulty[select.state].count;
         this.renderGame();
       } else {
         new Error(
@@ -437,6 +490,7 @@ class AppState {
     timer.innerHTML = "Оставшееся время: " + this.state.time;
     scoreBoard.append(timer);
 
+    // Score
     const score = document.createElement("p");
     score.id = "score";
     score.style.textAlign = "center";
@@ -481,6 +535,8 @@ class AppState {
       this.checkPreviewSize();
       this.cube.style.width = this.previewSize + "px";
       this.cube.style.height = this.previewSize + "px";
+
+      // this.cube.style.paddingTop = this.previewSize + "px";
     };
   };
 
@@ -516,32 +572,32 @@ class AppState {
     const game = document.createElement("div");
     game.id = "game";
     document.body.appendChild(game);
-  
+
     this.board = document.createElement("div");
     this.board.id = "game-board";
+    // Check width of the score board and set preview size
     this.scoreBoard = document.createElement("div");
     this.scoreBoard.id = "score-board";
     this.cubeSize = this.state.size;
-  
+
     const timer = document.createElement("p");
     timer.id = "timer";
     timer.style.textAlign = "center";
-  
-    // Переместите timer перед добавлением game-board и score-board
-    document.getElementById("game").appendChild(timer);
-    document.getElementById("game").appendChild(this.scoreBoard);
+
     document.getElementById("game").appendChild(this.board);
-  
+    document.getElementById("game").appendChild(this.scoreBoard);
+
+    // button
+
     const rotate = this.cubeCount > 10 ? true : false;
     this.rotate = rotate;
     this.game_ = new Game(this.cubeCount, this.cubeSize, this.trigger, rotate);
     this.SCOREBOARD(this.cubeSize);
-  
+
     if (!this.timer) {
       this.timer = new Timer(this.state.time, this.UPDATE_TIMER);
     }
   };
-  
 
   renderEnd = () => {
     document.body.innerHTML = endHTML;
@@ -582,9 +638,8 @@ class AppState {
       board.appendChild(p);
     }
 
-    this.renderResetButton("Таблица лидеров", "Вернуться в главное меню");
+    this.renderResetButton("leaderboard", "Go back to main menu");
   };
 }
 
 const appState = new AppState();
-
