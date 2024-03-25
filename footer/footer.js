a061988b8207d1e29ba0f84885e7afc1bb3b8402
ilyakanavin <ilyakanavin9@gmail.com>
2024-03-25 14:55:23 +0800

    only game

--- footer/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('./footer/footer.html') // Убедитесь, что путь указан правильно
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-container').innerHTML = data;
-        });
-});
-
